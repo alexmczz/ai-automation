33c90eea0ebbe217cc95f5ae5efe13931bc270ab
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-06-14 19:54:06 -0400

    Add files via upload

---
+++ ai_automation.py
@@ -0,0 +1,23 @@
+import openai
+import os
+
+openai.api_key = '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'
+
+
+while True:
+    user_input = input("> ")
+    if user_input.lower() == 'quit':
+        break
+    elif user_input.lower() == 'clear':
+        os.system('clear')
+    prompt = f'without comentary please type out a bash script that will {user_input}\n'
+    response = openai.Completion.create(
+        engine='text-davinci-003',
+        prompt=prompt,
+        max_tokens=100,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+    ai_response = response.choices[0].text.strip()
+    os.system(ai_response)
