efce935316dccaa58123f2384083b6f465db1e49
Alex McPadden <42503691+alexmczz@users.noreply.github.com>
2023-06-15 15:46:09 -0400

    Update ai_automation.py
    adding credit to GPT as I am using their ai model for the meantime.

--- ai_automation.py
+++ ai_automation.py
@@ -1,11 +1,16 @@
+#This project utilizes the GPT library provided by OpenAI (https://openai.com) 
+#for generating AI responses. The AI responses are generated using the OpenAI GPT-3.5 model. 
+# Please note that the AI responses are powered by OpenAI GPT-3.5 and not by my custom AI model, 
+# which is currently under development.
+
 import openai
 import os
 
-openai.api_key = '<openai api key>'
+openai.api_key = '<gpt api key goes here>'
 
 
 while True:
-    user_input = input("> ")
+    user_input = input("==User=> ")
     if user_input.lower() == 'quit':
         break
     elif user_input.lower() == 'clear':
